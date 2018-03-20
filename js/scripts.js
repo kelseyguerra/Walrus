94a0cf487adb132d2ca50be5364cdde2b380d5d2
Ami Cooper and Kelsey guerra <kelsey@>
2018-03-20 09:22:40 -0700

    create walrus website. add effects

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".walrus-showing").slideToggle();
+    $(".walrus-hidden").slideToggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".dog-showing").slideToggle();
+    $(".dog-hidden").slideToggle();
+  });
+});
